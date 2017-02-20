5301760632d76814d94c9f26b4853ed645c1e965
neutrino1911 <mirex1911@yandex.ru>
2017-02-21 00:24:48 +0500

    saf

--- src/helpers/functions/FileHelper.java
+++ src/helpers/functions/FileHelper.java
@@ -7,13 +7,11 @@ import library.AppData;
 
 import java.io.*;
 
-/**
- * Created by Святослав on 06.10.2016.
- */
 public class FileHelper {
 
 	public static void readData() {
 		AppData.getLists().add(new WList(0, Helper.getI18nString("allWords")));
+
 		if (!AppData.getData().exists()) {
 			try {
 				AppData.getData().createNewFile();
@@ -27,10 +25,7 @@ public class FileHelper {
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(AppData.getData()), "UTF-8"))) {
 			StringBuffer s = new StringBuffer();
 			s.append(reader.readLine());
-			if (s == null) {
-				Helper.showError("Read Error, can't read 'leng'\r\nCurrent line in file data.txt is empty!\r\nreadData() was stoped");
-				return;
-			}
+
 			if (!s.toString().equals("ru") && !s.toString().equals("en") && !s.toString().equals("ukr")) {
 				Helper.showError("Read Error, can't find 'leng'\r\nreadData() was stoped");
 				return;
@@ -38,10 +33,7 @@ public class FileHelper {
 			AppData.getSettings().setLeng(s.toString());
 			s.delete(0, s.length());
 			s.append(reader.readLine());
-			if (s == null) {
-				Helper.showError("Read Error, can't read 'tran'\r\nCurrent line in file data.txt is empty!\r\nreadData() was stoped");
-				return;
-			}
+
 			if (!s.toString().equals("ukr") && !s.toString().equals("ru")) {
 				Helper.showError("Read Error\r\nCan't find tran\r\nreadData() was stoped");
 				return;
@@ -49,10 +41,7 @@ public class FileHelper {
 			AppData.getSettings().setTran(s.toString());
 			s.delete(0, s.length());
 			s.append(reader.readLine());
-			if (s == null) {
-				Helper.showError("Read Error, can't find '__________' after tran\r\nCurrent line in file data.txt is empty!\r\nreadData() was stoped");
-				return;
-			}
+
 			if (!s.toString().equals("__________")) {
 				Helper.showError("Read Error, can't find '__________' after tran\r\nCurrent line isn't equals to '__________'\r\nreadData() was stoped");
 				return;
@@ -61,17 +50,14 @@ public class FileHelper {
 			while (true) {
 				s.delete(0, s.length());
 				s.append(reader.readLine());
-				if (s == null) {
-					Helper.showError("data.txt was finished before '__________'\r\nfon list reading\r\nreadData() was stoped");
-					return;
-				}
+
 				if (s.toString().equals("__________")) break;
 				if (s.indexOf("/") == -1) {
 					Helper.showError("Read Error\r\n'/' is in -1 position in list reading\r\nreadData() was stoped");
 					return;
 				}
 				String key = s.substring(0, s.indexOf("/"));
-				if (AppData.getListkeys()[Integer.parseInt(key)] == true)
+				if (AppData.getListkeys()[Integer.parseInt(key)])
 					Helper.showError("ListKey" + key + "was already used!");
 				s.delete(0, s.indexOf("/") + 1);
 				AppData.getLists().add(new WList(Integer.parseInt(key), s.toString()));
@@ -80,13 +66,10 @@ public class FileHelper {
 			while (true) {
 				s.delete(0, s.length());
 				s.append(reader.readLine());
-				if (s == null) {
-					Helper.showError("data.txt was finished before '__________'\r\non words reading\r\nreadData() was stoped");
-					return;
-				}
+
 				if (s.toString().equals("__________")) break;
 				Word w = new Word(s);
-				if (AppData.getWordkeys()[w.getKey()] == true)
+				if (AppData.getWordkeys()[w.getKey()])
 					Helper.showError("WordKey " + w.getKey() + "was already used!");
 				AppData.getWordkeys()[w.getKey()] = true;
 				s.delete(0, s.lastIndexOf("/") + 1);
@@ -127,15 +110,15 @@ public class FileHelper {
 	}
 
 	public static void rewrite() {																														// перезаписывает файл данных
-		try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(AppData.getData()), "UTF-8"))) {
+		try (FileWriter writer = new FileWriter(AppData.getData())) {
 			LList lists = AppData.getLists();
-			writer.write(AppData.getSettings().getLeng() + "\r\n");
-			writer.write(AppData.getSettings().getTran() + "\r\n");
-			writer.write("__________\r\n");
+			writer.write(AppData.getSettings().getLeng() + "\n");
+			writer.write(AppData.getSettings().getTran() + "\n");
+			writer.write("__________\n");
 			for (int i = 1; i < lists.getLists().size(); i++) {
-				writer.write(lists.get(i).getKey() + "/" + lists.get(i).getName() + "\r\n");
+				writer.write(lists.get(i).getKey() + "/" + lists.get(i).getName() + "\n");
 			}
-			writer.write("__________\r\n");
+			writer.write("__________\n");
 			for (int i = 0; i < lists.get(0).getWords().size(); i++) {
 				writer.write(lists.get(0).get(i).getKey() + "/" + lists.get(0).get(i).getEng() + "/" + lists.get(0).get(i).getRus() + "/" + lists.get(0).get(i).getUkr() + "/");
 				for (int j = 1; j < lists.getLists().size(); j++)
@@ -143,7 +126,7 @@ public class FileHelper {
 						if (lists.get(j).getWords().get(k).getKey() == lists.get(0).get(i).getKey())
 							writer.write(lists.get(j).getKey() + " ");
 					}
-				writer.write("0\r\n");
+				writer.write("0\n");
 			}
 			writer.write("__________");
 			writer.flush();
@@ -159,47 +142,22 @@ public class FileHelper {
 		}
 	}
 
-	public static void prewrite() {																						// заполняет файл данных значениями по умолчанию
-		try {
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(AppData.getData()), "UTF-8"));
-			writer.write("ru");
-			writer.newLine();
-			writer.write("ru");
-			writer.newLine();
-			writer.write("__________");
-			writer.newLine();
-			writer.write("__________");
-			writer.newLine();
-			writer.write("__________");
-			writer.close();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			Helper.showException("UnsupportedEncodingException in FileHelper.prewrite()", e);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			Helper.showException("FileNotFoundException in FileHelper.prewrite()", e);
+	public static void prewrite() {//заполняет файл данных значениями по умолчанию
+		try(FileWriter writer = new FileWriter(AppData.getData())) {
+			writer.write("ru\n");
+			writer.write("ru\n");
+			writer.write("__________\n");
+			writer.write("__________\n");
+			writer.write("__________\n");
 		} catch (IOException e) {
 			e.printStackTrace();
 			Helper.showException("IOException e in FileHelper.prewrite()", e);
 		}
 	}
 
-	public static void writeStringToFile(String s, File f) {															// вызывается из Helper.combineTranslates() используется для записи ключ-значение в файл
-		if (!f.exists()) try {
-			f.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
-			Helper.showException(e.getMessage()+"\n"+e.toString(), e);
-		}
-		try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "UTF-8"))) {
+	public static void writeStringToFile(String s, File f) {//вызывается из Helper.combineTranslates() используется для записи ключ-значение в файл
+        try (FileWriter writer = new FileWriter(f)) {
 			writer.write(s);
-			writer.close();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			Helper.showException("UnsupportedEncodingException in FileHelper.writeStringToFile(String s, File f)", e);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			Helper.showException("FileNotFoundException in FileHelper.writeStringToFile(String s, File f)", e);
 		} catch (IOException e) {
 			e.printStackTrace();
 			Helper.showException("IOException e in FileHelper.writeStringToFile(String s, File f)", e);
